3d1f11aba137072fdb5f8a2dd074189f7702847f
Ruby <rubyist@vcbln0002.fritz.box>
2012-12-30 23:27:17 +0100

    added Highchart for chart rendering

--- web/web_rnexus.rb
+++ web/web_rnexus.rb
@@ -1,8 +1,14 @@
 require 'bundler/setup'
+require 'erb'
 require 'sinatra'
 require 'haml'
 require 'rnexus'
 
 get '/' do
+  @db = Rnexus::DBManager.new('db')
+  temp_data = Rnexus::DBManager::Measurement.all.map(&:T5)
+  chart_title = "Outside Temperature"
+  erb = ERB.new(File.read("web/temperature_line.js.erb"))
+  @temperature_line_js = erb.result(binding)
   haml :index
 end
