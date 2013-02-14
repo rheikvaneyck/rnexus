484fc1bf59db484b9f47fb410bb11f613674b125
Rheik van Eyck <rheikvaneyck@yahoo.de>
2013-02-14 22:12:59 +0100

    edited diagrams

--- web/web_rnexus.rb
+++ web/web_rnexus.rb
@@ -29,15 +29,28 @@ weather_forcast:
   @status = Rnexus::Status.new('config')
   @state = @status.get_last_state
   @batteries = [battery_states[@state.BAT1],battery_states[@state.BAT5],battery_states[@state.BATR],battery_states[@state.BATW]]
+  
   temp_data = @plotter.get_last_24h(:T5).map {|d| [DateTime.parse(d[0]).to_time.to_i * 1000, d[1]] }
   temp_values = temp_data.inspect
+  
   press_data =  @plotter.get_last_24h(:PRESS).map {|d| d[1] }
   press_values = press_data.inspect
+  
   humaditity_data =  @plotter.get_last_24h(:H5).map {|d| [DateTime.parse(d[0]).to_time.to_i * 1000, d[1]] }
   humaditity_values = humaditity_data.inspect
-	rain_data = @plotter.get_last_24h(:RC)
-	rain_value = rain_data.first[1] - rain_data.last[1]
-	puts rain_value
+	
+  rain_data = @plotter.get_last_24h(:RC)
+	rain_value = rain_data.last[1] - rain_data.first[1]
+  
+  wind_direction = @plotter.get_last_24h(:WD).map {|d| d[1] }
+  wind_speed = @plotter.get_last_24h(:WS).map {|d| d[1] }
+  # FIXME CALC wind array (prob with array.zip()? 
+  wind = wind_direction.zip(wind_speed)
+  wind_groups = wind.group_by {|w| w[0] }
+  wind_sums = Array.new(16,0) 
+  wind_groups.each {|k,v| sum = 0.0; v.map {|i| sum += i[1].to_f }; wind_sums[k.to_i - 1] = (sum / v.length) }
+  wind_values = wind_sums.inspect
+
   start_unixtime = temp_data.first[0]
   chart_title = "Last 24h"
   erb = ERB.new(File.read("web/temperature_line.js.erb"))
@@ -46,6 +59,8 @@ weather_forcast:
   @rain_gauge_js = erb.result(binding)  
   erb = ERB.new(File.read("web/press_column.js.erb"))
   @press_column_js = erb.result(binding)  
+  erb = ERB.new(File.read("web/wind_polar.js.erb"))
+  @wind_polar_js = erb.result(binding)  
 
   haml :index
 end
