432f38864e456011858e2a95e7335d87ed640e0f
Rheik van Eyck <rheikvaneyck@yahoo.de>
2013-03-29 23:05:32 +0100

    fixed pg connection

--- lib/rnexus/measurement.rb
+++ lib/rnexus/measurement.rb
@@ -4,13 +4,29 @@
 # Licensed under the same terms as Ruby. No warranty is provided.
 
 require 'active_record'
+require 'uri'
 require 'yaml'
 require 'logger'
 
 module Rnexus
   class DBManager
     def initialize(config_path)
-      db_config = YAML::load(File.open(File.join(config_path,'database.yml')))     
+      if (ENV['DATABASE_URL']) then
+        db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/app-dev')
+        db_config = {
+          :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+          :host     => db.host,
+          :port     => db.port,
+          :username => db.user,
+          :password => db.password,
+          :database => db.path[1..-1],
+          :encoding => 'utf8',
+          :log_dir => "log"
+        }
+      else
+        db_config = YAML::load(File.open(File.join(config_path,'database.yml')))
+      end
+
       ActiveRecord::Base.establish_connection(db_config)
       log_dir = db_config["log_dir"]
       ActiveRecord::Base.logger = Logger.new(File.open(File.join(log_dir,'database.log'), 'a'))
