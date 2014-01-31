8e42b627d2b41f6efd1424843e0e740bca788e43
Rheik van Eyck <rheikvaneyck@yahoo.de>
2014-01-31 13:52:25 +0100

    Update application_controller.rb

--- web/controllers/application_controller.rb
+++ web/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < Sinatra::Base
 	helpers ApplicationHelper
-
+	
+	set :root, File.expand_path('../web', __FILE__)
 	set :views, File.expand_path('../../views', __FILE__)
   	set :public_folder, File.expand_path('../../public', __FILE__)
 
